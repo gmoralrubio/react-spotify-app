d20b1711b8042dcc81f8bcc3e68c9b015250e2ba
Guillermo-moral <70134524+Guillermo-moral@users.noreply.github.com>
2022-08-16 19:21:51 +0200

    añadido metodo de refresco del token

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -8,13 +8,9 @@ export default function useAuth(code) {
 
   useEffect(() => {
     axios
-      .post(
-        'http://localhost:3001/callback',
-        {
-          code,
-        },
-        { headers: { 'Content-Type': 'application/json' } }
-      )
+      .post('http://localhost:3001/login', {
+        code,
+      })
       .then(res => {
         setAccessToken(res.data.accessToken)
         setRefreshToken(res.data.refreshToken)
@@ -25,6 +21,26 @@ export default function useAuth(code) {
         window.location = '/'
         console.log(error)
       })
-  }, [code])
+  }, [])
+
+  useEffect(() => {
+    if (!refreshToken || !expiresIn) return
+    const interval = setInterval(() => {
+      axios
+        .post('http://localhost:3001/refresh', {
+          refreshToken,
+        })
+        .then(res => {
+          setAccessToken(res.data.accessToken)
+          setExpiresIn(res.data.expiresIn)
+        })
+        .catch(() => {
+          window.location = '/'
+        })
+    }, (expiresIn - 60) * 1000)
+
+    return () => clearInterval(interval)
+  }, [refreshToken, expiresIn])
+
   return accessToken
 }
