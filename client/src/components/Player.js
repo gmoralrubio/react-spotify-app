ee07c4286ca286ef87fc7de502d3244c99a6e1c7
Guillermo-moral <70134524+Guillermo-moral@users.noreply.github.com>
2022-08-16 19:21:20 +0200

    cambio acceso parametros del player

--- client/src/components/Player.js
+++ client/src/components/Player.js
@@ -1,11 +1,5 @@
-import React from 'react'
 import SpotifyPlayer from 'react-spotify-web-playback'
-import { useParams } from 'react-router-dom'
 
-export default function Player() {
-  const { accessToken, uri } = useParams()
-  console.log('accessToken', accessToken)
-  return (
-    <SpotifyPlayer token={accessToken} uris={['spotify:artist:6HQYnRM4OzToCYPpVBInuU']} />
-  )
+export default function Player({ accessToken, trackUri }) {
+  return <SpotifyPlayer token={accessToken} uris={trackUri ? [trackUri] : null} />
 }
