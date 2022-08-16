8a20a72809accc03134e423b7b65fcc03da44a2b
Guillermo-moral <70134524+Guillermo-moral@users.noreply.github.com>
2022-08-16 19:20:33 +0200

    query datos del perfil del usuario

---
+++ client/src/services/getUserProfile.js
@@ -0,0 +1,14 @@
+import { BASE_API_URL } from '../config'
+
+export default async function getUserProfile(accessToken) {
+  const queryParameters = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${accessToken}`,
+    },
+  }
+  let response = await fetch(`${BASE_API_URL}/me`, queryParameters)
+  let result = await response.json()
+  return result
+}
