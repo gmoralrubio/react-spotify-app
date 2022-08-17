86894e7a556c5e58741ad083b9b8ff8398a1ca56
Guillermo-moral <70134524+Guillermo-moral@users.noreply.github.com>
2022-08-17 17:21:17 +0200

    icluido el container del swiper en el componente

--- client/src/components/CustomSwiper.js
+++ client/src/components/CustomSwiper.js
@@ -8,52 +8,57 @@ import AlbumCard from './AlbumCard'
 import ArtistCard from './ArtistCard'
 import PlaylistCard from './PlayListCard'
 import TrackCard from './TrackCard'
+import { capitalizeFirstLetter } from './../helpers'
 
 export default function CustomSwiper({ type, slides }) {
   return (
-    <Swiper
-      modules={[Pagination, Mousewheel]}
-      spaceBetween={10}
-      slidesPerView={2}
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={swiper => {}}
-      mousewheel={true}
-      direction={'horizontal'}
-      pagination={{
-        dynamicBullets: true,
-      }}
-      breakpoints={{
-        320: {
-          slidesPerView: 2,
-          spaceBetween: 10,
-        },
-        480: {
-          slidesPerView: 3,
-          spaceBetween: 10,
-        },
-        640: {
-          slidesPerView: 4,
-          spaceBetween: 10,
-        },
-        768: {
-          slidesPerView: 5,
-          spaceBetween: 10,
-        },
-        1024: {
-          slidesPerView: 6,
-          spaceBetween: 10,
-        },
-      }}
-    >
-      {slides &&
-        slides.items.map(item => (
-          <SwiperSlide key={item.id}>
-            {type === 'albums' && <AlbumCard item={item} />}
-            {type === 'artists' && <ArtistCard item={item} />}
-            {type === 'playlists' && <PlaylistCard item={item} />}
-            {type === 'tracks' && <TrackCard item={item} />}
-          </SwiperSlide>
-        ))}
-    </Swiper>
+    <div className="rounded-md bg-slate-700 p-4">
+      <h2 className="mb-4 text-xl font-bold text-white">{capitalizeFirstLetter(type)}</h2>
+      <Swiper
+        modules={[Pagination, Mousewheel]}
+        spaceBetween={10}
+        slidesPerView={2}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={swiper => {}}
+        mousewheel={true}
+        direction={'horizontal'}
+        pagination={{
+          dynamicBullets: true,
+        }}
+        breakpoints={{
+          320: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+          },
+          480: {
+            slidesPerView: 3,
+            spaceBetween: 10,
+          },
+          640: {
+            slidesPerView: 4,
+            spaceBetween: 10,
+          },
+          768: {
+            slidesPerView: 5,
+            spaceBetween: 10,
+          },
+          1024: {
+            slidesPerView: 6,
+            spaceBetween: 10,
+          },
+        }}
+      >
+        {slides &&
+          slides.items.map(item => (
+            <SwiperSlide key={item.id}>
+              {type === 'albums' && <AlbumCard item={item} />}
+              {type === 'artists' ||
+                (type === 'top artists' && <ArtistCard item={item} />)}
+              {type === 'playlists' && <PlaylistCard item={item} />}
+              {type === 'tracks' || (type === 'top tracks' && <TrackCard item={item} />)}
+            </SwiperSlide>
+          ))}
+      </Swiper>
+    </div>
   )
 }
