2a0a9e0f35c2555e04b839a3f30fb5f101099c4f
Guillermo-moral <70134524+Guillermo-moral@users.noreply.github.com>
2022-08-18 18:16:41 +0200

    player implementado

--- client/src/components/Player.js
+++ client/src/components/Player.js
@@ -1,5 +1,26 @@
 import SpotifyPlayer from 'react-spotify-web-playback'
+import { useApi } from './../context/ApiContext'
 
-export default function Player({ accessToken, trackUri }) {
-  return <SpotifyPlayer token={accessToken} uris={trackUri ? [trackUri] : null} />
+export default function Player() {
+  const { accessToken, playTrack, initialPlayTrack } = useApi()
+  return (
+    <>
+      {initialPlayTrack && (
+        <SpotifyPlayer
+          token={accessToken}
+          uris={playTrack ? [playTrack] : [initialPlayTrack]}
+          play={playTrack ? true : false}
+          magnifySliderOnHover={true}
+          styles={{
+            bgColor: '#0f172a',
+            color: '#ffffff',
+            sliderColor: '#f43f5e',
+            sliderTrackColor: '#881337',
+            trackNameColor: '#ffffff',
+            trackArtistColor: '#ffffff',
+          }}
+        />
+      )}
+    </>
+  )
 }
