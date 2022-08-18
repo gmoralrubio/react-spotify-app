ec009506fc0210da032ac00a4100b361378fe455
Guillermo-moral <70134524+Guillermo-moral@users.noreply.github.com>
2022-08-18 18:44:35 +0200

    reproduccion de canciones de un album

--- client/src/pages/AlbumTracksPage.js
+++ client/src/pages/AlbumTracksPage.js
@@ -4,11 +4,13 @@ import getAlbumTracks from '../services/getAlbumTracks'
 import { millisToMinutesAndSeconds } from '../helpers'
 import { useApi } from './../context/ApiContext'
 import Player from './../components/Player'
+import SearchInput from '../components/SearchInput'
 
 export default function AlbumTracksPage() {
   const [albumTracks, setAlbumTracks] = useState()
+  // REVISAR
   let { albumId, albumName } = useParams()
-  const { accessToken } = useApi()
+  const { accessToken, setPlayTrack } = useApi()
 
   useEffect(() => {
     getAlbumTracks(albumId, accessToken)
@@ -16,13 +18,30 @@ export default function AlbumTracksPage() {
       .then(console.log('albumTracks', albumTracks))
   }, [albumId])
 
+  const clickHandler = trackUri => {
+    setPlayTrack(trackUri)
+  }
+
   return (
-    <div className="relative">
+    <div className="relative mb-20">
       {accessToken && (
         <div className="fixed bottom-0 left-0 z-50 w-full">
           <Player />
         </div>
       )}
+
+      <div className="mt-10">
+        <h1 className="mb-4 text-6xl font-bold text-white sm:text-8xl">
+          {albumTracks && albumTracks.items[0].artists[0].name}
+        </h1>
+        <h2 className=" mb-10 text-4xl font-light text-white sm:text-6xl">{albumName}</h2>
+      </div>
+
+      {/* TODO: implementar busqueda */}
+      <div className="mb-10">
+        <SearchInput />
+      </div>
+
       <div className="flex flex-col gap-2">
         <div className="grid grid-cols-6 p-2 font-bold text-white">
           <span className="col-span-2">Title</span>
@@ -32,20 +51,18 @@ export default function AlbumTracksPage() {
         </div>
         {albumTracks &&
           albumTracks.items.map(track => (
-            <Link
+            <div
+              className="grid cursor-pointer grid-cols-6 p-2 text-white hover:bg-slate-900/20"
               key={track.id}
-              to={`/player/${accessToken}/${track.uri}`}
-              className="cursor-pointer"
+              onClick={() => clickHandler(track.uri)}
             >
-              <div className="grid grid-cols-6 p-2 text-white hover:bg-slate-900/20">
-                <span className="col-span-2">{track.name}</span>
-                <span className="col-span-2 text-center">{albumName}</span>
-                <span className="text-center">{track.artists[0].name}</span>
-                <span className="text-right">
-                  {millisToMinutesAndSeconds(track.duration_ms)}
-                </span>
-              </div>
-            </Link>
+              <span className="col-span-2">{track.name}</span>
+              <span className="col-span-2 text-center">{albumName}</span>
+              <span className="text-center">{track.artists[0].name}</span>
+              <span className="text-right">
+                {millisToMinutesAndSeconds(track.duration_ms)}
+              </span>
+            </div>
           ))}
       </div>
     </div>
