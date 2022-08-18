12ae23ffbe30df243f9e7fd9bdc59c72f626b6af
Guillermo-moral <70134524+Guillermo-moral@users.noreply.github.com>
2022-08-18 20:56:55 +0200

    linkado con la pagina de albums por artista

--- client/src/components/ArtistCard.js
+++ client/src/components/ArtistCard.js
@@ -1,14 +1,19 @@
 import Card from './common/Card'
+import { Link } from 'react-router-dom'
 
 export default function ArtistCard({ item }) {
   return (
-    <Card className="h-[250px] items-stretch gap-4 p-4">
-      <img
-        className="aspect-square rounded-full object-cover"
-        src={item.images[0] ? item.images[0].url : 'https://via.placeholder.com/640x640'}
-        alt=""
-      />
-      <h3 className="font-bold">{item.name}</h3>
-    </Card>
+    <Link to={`/artist-albums/${item.id}`}>
+      <Card className="h-[250px] items-stretch gap-4 p-4">
+        <img
+          className="aspect-square rounded-full object-cover"
+          src={
+            item.images[0] ? item.images[0].url : 'https://via.placeholder.com/640x640'
+          }
+          alt=""
+        />
+        <h3 className="font-bold">{item.name}</h3>
+      </Card>
+    </Link>
   )
 }
