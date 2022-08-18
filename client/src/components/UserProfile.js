44f24c8d204aedd1d3bcd315bd520321c9521d28
Guillermo-moral <70134524+Guillermo-moral@users.noreply.github.com>
2022-08-18 18:17:49 +0200

    tabulado correcto

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -1,13 +1,12 @@
-
-export default function UserProfile({userProfile}) {
+export default function UserProfile({ userProfile }) {
   return (
     <div className="mb-10 flex items-center gap-6">
-            <div className="w-40 ">
-              <img className="rounded-full" src={userProfile.images[0].url} alt="" />
-            </div>
-            <h1 className="py-10 text-4xl font-bold text-white sm:text-6xl">
-              {userProfile.display_name}
-            </h1>
-          </div>
+      <div className="w-40 ">
+        <img className="rounded-full" src={userProfile.images[0].url} alt="" />
+      </div>
+      <h1 className="py-10 text-4xl font-bold text-white sm:text-6xl">
+        {userProfile.display_name}
+      </h1>
+    </div>
   )
 }
